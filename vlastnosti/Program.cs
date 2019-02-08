6e7ce198827aa4330ce812d0f2914b7fe4c17dd0
batlicka <46314552+batlicka@users.noreply.github.com>
2019-02-08 14:07:30 +0100

    basic commit

---
+++ vlastnosti/Program.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vlastnosti
+{
+    public class Bod
+    {
+        private short m_x;
+        private short m_y;
+        public Bod() { m_x = m_y = 0; }
+        public Bod (short nx, short ny) { m_x = nx; m_y = ny; }
+        public short x
+        {
+            get { return m_x; }
+            set
+            {
+                if (value < 0)
+                    m_x = 0;
+                else
+                    m_x = value;
+            }
+        }
+        public short y
+        {
+            get { return m_y; }
+            set
+            {
+                if (value < 0)
+                    m_y = 0;
+                else
+                    m_y = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //příklad z knihy
+            Bod b = new Bod();
+            b.x = 1;
+            b.y = 2;
+            Console.WriteLine("x={0}, y={1}", b.x, b.y);
+
+            //příklad z internetu
+            Student s = new Student("Pavel Hora", true, 20);
+            s.Vek = 15;
+            s.Jmeno = "Vasek Vagunda";            
+            // s.Muz = false; // tento řádek nyní způsobí chybu a musí být odebrán
+            Console.WriteLine(s);
+
+        }
+    }
+}
---
+++ vlastnosti/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vlastnosti
+{
+    class Student
+    {
+        public string Jmeno { get;  set; }
+        public bool Muz { get; set; }
+        public bool Plnolety { get;  set; }
+        private int vek;
+        public int Vek
+        {
+            get
+            {
+                return vek;
+            }
+            set
+            {
+                vek = value;
+                // kontrola plnoletosti
+                Plnolety = true;
+                if (vek < 18)
+                    Plnolety = false;
+            }
+        }
+
+        public Student(string jmeno, bool pohlavi, int vek)
+        {
+            EditujStudenta(jmeno, pohlavi, vek);
+        }
+
+        public void EditujStudenta(string jmeno, bool pohlavi, int vek)
+        {
+            Jmeno = jmeno;
+            Muz = pohlavi;
+            Vek = vek;
+        }
+
+        public override string ToString()
+        {
+            string jsemPlnolety = "jsem";
+            if (!Plnolety)
+                jsemPlnolety = "nejsem";
+            string pohlavi = "muž";
+            if (!Muz)
+                pohlavi = "žena";
+            return String.Format("Jsem {0}, {1}. Je mi {2} let a {3} plnoletý.", Jmeno, pohlavi, Vek, jsemPlnolety);
+        }
+    }
+}
